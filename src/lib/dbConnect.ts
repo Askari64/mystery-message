d41eccef53d75392476a082d2f368f44af2dbfb4
Askari <askarir64@gmail.com>
2025-01-20 21:45:41 +0530

    db connection file

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type connectionState = {
+    isConnected?: number;
+};
+
+const connection: connectionState = {};
+
+async function dbConnect() {
+    if (connection.isConnected) {
+        console.log("🟡 Database already connected");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || "");
+        connection.isConnected = db.connections[0].readyState;
+
+        console.log("🟢 Database Successfully Conncted");
+    } catch (error) {
+        console.log(`🔴 Error Connecting Database: ${error}`);
+
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
